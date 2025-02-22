2b362195475eb20e631a735a2b6fb37b94e495dd
GoldOneS6998 <ernestopb.ti23@utsjr.edu.mx>
2025-02-22 00:38:32 -0600

    Update usuarioModelo.js

--- models/usuarioModelo.js
+++ models/usuarioModelo.js
@@ -1,31 +1,37 @@
 import mongoose from "mongoose";
 
-const usuariosSchema =new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:true
+const usuariosSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
     },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true
     },
-    salt:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:true
+    salt: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    tipoUsuario: {
+        type: String,
+        enum: ["usuario", "admin"],  // Solo puede ser "usuario" o "admin"
+        default: "usuario",  // Valor por defecto si no se especifica
+        required: true
     }
 },
 {
-   timestamps:true 
+    timestamps: true  // Esto agrega las fechas de creación y actualización automáticamente
 });
 
 export default mongoose.model('User', usuariosSchema);
